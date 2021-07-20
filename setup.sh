db8ee17e624bd6f31e5a9b60faedf02ee90ab4d5
Fernando Rodriguez <frodriguez@Fernandos-MacBook-Pro.local>
2021-07-20 15:24:52 -0500

    change .git location

---
+++ setup.sh
@@ -0,0 +1,10 @@
+export APP_SECRET_KEY="pN1qArB6GIPgB1N_EBrCBzrgtZHsa1bI0CPPGQlgQArS7nztazgJUxnov6Gg7PRD"
+
+export DATABASE_URL="postgres://postgres@localhost:5432/capstone"
+
+export AUTH_AUDIENCE="courses"
+export AUTH_CALL_BACK_URL="http://127.0.0.1:5000/callback"
+export AUTH_CLIENT_ID="7HMxHXT4PH7JEoAyhC9nU3kxKCEPz8ln"
+export AUTH_URL="nandodev.us"
+
+export PYTHONPATH='/Users/frodriguez/Desktop/my-apps/capstone-project'
