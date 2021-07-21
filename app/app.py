b11552c3f6987e5a17f6db22af3482a8845d8ed6
Fernando Rodriguez <frodriguez@Fernandos-MacBook-Pro.local>
2021-07-21 11:49:37 -0500

    fix init

--- app/app.py
+++ app/app.py
@@ -10,7 +10,7 @@ from flask import (
     session
     )
 
-from app.models import (
+from .models import (
     db,
     setup_db,
     Subject,
@@ -19,7 +19,7 @@ from app.models import (
     )
 from flask_cors import CORS
 from six.moves.urllib.parse import urlencode
-from app.functions import (
+from .functions import (
     format_date,
     build_login_link,
     validate_json_keys,
