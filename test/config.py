d823af6c8fbaec4abc5e523a806acbb14f48d0e0
Fernando Rodriguez <frodriguez@Fernandos-MacBook-Pro.local>
2021-07-23 13:13:53 -0500

    change file structure

--- test/config.py
+++ test/config.py
@@ -3,6 +3,6 @@ from unittest.mock import patch
 import json
 from flask_sqlalchemy import SQLAlchemy
 
-from app.app import create_app
+from app import create_app
 from app.models import setup_db, subject_student, Subject, Student
 from app.functions import query_a_record
